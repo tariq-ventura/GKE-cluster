cc597f56a95dc9693da358b15465266c97f1ed9f
tariq-ventura <149212441+tariq-ventura@users.noreply.github.com>
2023-11-27 05:50:34 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    https: true,
     allowedHosts: 'all',
   },
   configureWebpack: {
