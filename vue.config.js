ef829fe7b629a6f91208d271f590fca11e560232
tariq-ventura <149212441+tariq-ventura@users.noreply.github.com>
2023-11-26 22:10:18 -0600

    allow https

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    https: true,
     allowedHosts: 'all',
   },
   configureWebpack: {
